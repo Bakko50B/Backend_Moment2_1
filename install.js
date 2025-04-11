80689f7029b80450901a43964e5face2d8927cd7
Torbjörn Lundberg <torbjorn@basstrombone.info>
2025-04-11 14:55:50 +0200

    skapar tabell och data i install.js Route Post för inmatning av data

--- install.js
+++ install.js
@@ -27,13 +27,61 @@ connection.query("CREATE DATABASE IF NOT EXISTS cv", (error, results) => {
     }
 });
 
-connection.query("CREATE TABLE IF NOT EXISTS `cv`.`workexp` (`id` INT NOT NULL AUTO_INCREMENT , `companyname` INT(64) NOT NULL , `jobtitle` INT(64) NOT NULL , `location` INT(64) NOT NULL , `startdate` INT NOT NULL , `enddate` INT NULL , `description` INT NULL , `postcreated` INT NOT NULL DEFAULT CURRENT_TIMESTAMP , PRIMARY KEY (`id`)) ENGINE = InnoDB;;", 
+// connection.query("CREATE TABLE IF NOT EXISTS `cv`.`workexp` (`id` INT NOT NULL AUTO_INCREMENT , `companyname` INT(64) NOT NULL , `jobtitle` INT(64) NOT NULL , `location` INT(64) NOT NULL , `startdate` INT NOT NULL , `enddate` INT NULL , `description` INT NULL , `postcreated` INT NOT NULL DEFAULT CURRENT_TIMESTAMP , PRIMARY KEY (`id`)) ENGINE = InnoDB;;", 
+//     (error, result) => {
+//         if (error) {
+//             throw error;
+//         }
+//         else {
+//             console.log("Database table created");
+//         }
+//     }
+// );
+
+connection.query(`
+    CREATE TABLE IF NOT EXISTS cv.workexp (
+        id INT NOT NULL AUTO_INCREMENT,
+        companyname VARCHAR(64) NOT NULL,
+        jobtitle VARCHAR(64) NOT NULL,
+        location VARCHAR(64) NOT NULL,
+        startdate DATE NOT NULL,
+        enddate DATE NULL,
+        description TEXT NULL,
+        postcreated TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+        PRIMARY KEY (id)
+    ) ENGINE = InnoDB 
+    DEFAULT CHARACTER SET utf8mb4 
+    COLLATE utf8mb4_swedish_ci;
+`, 
+(error, result) => {
+    if (error) {
+        throw error;
+    } else {
+        console.log('Database table created');
+    }
+});
+
+// Kör INSERTS till tabellen
+connection.query(
+    "INSERT INTO workexp (companyname, jobtitle, location, startdate, enddate, description) VALUES (?, ?, ?, ?, ?, ?)",
+    ['Tidningstjänst', 'Tidningsutdelare', 'Sandviken', '1980-06-15', '1980-07-05', 'Utdelning av morgontidningar i distrikt Klangberget, Sandviken'],
     (error, result) => {
         if (error) {
             throw error;
+        } else {
+            console.log('Row inserted into workexp table');
         }
-        else {
-            console.log("Database table created");
+    }
+);
+
+connection.query(
+    "INSERT INTO workexp (companyname, jobtitle, location, startdate, enddate, description) VALUES (?, ?, ?, ?, ?, ?)",
+    ['Tidningstjänst', 'Tidningsutdelare', 'Sandviken', '1981-06-13', '1981-07-15', 'Utdelning av morgontidningar i distrikt Klangberget, Sandviken'],
+    (error, result) => {
+        if (error) {
+            throw error;
+        } else {
+            console.log('Row inserted into workexp table');
         }
     }
 );
--- server.js
+++ server.js
@@ -4,21 +4,17 @@
  */
 const express = require('express');
 const cors = require('cors');
-//ENV
-require('dotenv').config();
+require('dotenv').config();         //ENV
+const mysql = require("mysql");     //Databasanslutning
 
 const app = express();
-const port = process.env.PORT || 3500;
-
-
-//Databasanslutning
-const mysql = require("mysql");
+const port = process.env.PORT || 3000;
 
 const connection = mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME   
+    database: process.env.DB_NAME
 });
 
 connection.connect((error) => {
@@ -30,23 +26,90 @@ connection.connect((error) => {
     }
 });
 
-// Använd JSON-data i anropen
-app.use(express.json());
-
-// Aktivera CORS middleware för alla rutter
-app.use(cors());
+app.use(express.json());            // Använd JSON-data i anropen
+app.use(cors());                    // Aktivera CORS middleware för alla rutter
 
 /** ------ Rutter (Routes) ------ */
-
 // GET /api
 app.get('/api', (req, res) => {
-    res.json({ message: 'Welcome to my copied REST API' });
+        //Get cv
+    connection.query('SELECT * FROM workexp', (error, results) => {
+        if (error) {
+            res.status(500).json({ ERROR: "Something went wrong: " + error });
+            return;
+        }
+        else {
+            if (results.length === 0) {
+                res.status(404).json({ message: "No workexperience found!" });
+            }
+            else {
+                res.json(results);
+            }
+        }
+    });
 });
 
 // GET /api/users
 app.get('/api/users', (req, res) => {
     res.json({ message: 'GET request to api/users' });
 });
+
+// POST /api/users
+app.post('/api', (req, res) => {
+    let companyname = req.body.companyname;
+    let jobtitle = req.body.jobtitle;
+    let location = req.body.location;
+    let startdate = req.body.startdate;
+    let enddate = req.body.enddate || "";
+    let description = req.body.description || "";
+
+    let errors = {
+        message: "",
+        detail: "",
+        http_response: {
+
+        }
+    };
+
+    if (!companyname || !jobtitle || !location || !startdate) {
+        errors.message = "All mandatory fields must be filled in!"
+        errors.detail = "Companyname, jobtitle, location and startdate need to be filled out before insertion into database!"
+
+        errors.http_response.message = "Bad request!";
+        errors.http_response.code = 400;
+
+        res.status(400).json(errors);
+
+        return
+    }
+
+    //Add workexperience to database
+    connection.query(`INSERT INTO workexp (companyname, jobtitle, location, startdate, enddate, description) VALUES (?, ?, ?, ?, ?, ?);`,
+        [companyname, jobtitle, location, startdate, enddate, description], (error, results) => {
+            if (error) {
+                res.status(500).json({ ERROR: "Something went wrong: " + error });
+                return;
+            }
+            else {
+                console.log("Query executed: " + JSON.stringify(results));
+        
+                let workexperience = {
+                    companyname: companyname,
+                    jobtitle: jobtitle,
+                    location: location,
+                    startdate: startdate,
+                    enddate: enddate,
+                    description: description
+                };
+        
+                res.json({ message: "New workexperience added", data: workexperience });
+            }
+        });
+    
+
+    // res.json({ message: 'POST request to api/users' });
+});
+
 
 // POST /api/users
 app.post('/api/users', (req, res) => {
